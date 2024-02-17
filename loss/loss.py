da9cfe6848844e1b2faf876e265dc83b1080fb31
Esteban RS <72273395+esteban-rs@users.noreply.github.com>
2024-02-16 21:32:24 -0600

    Update loss.py
    update to v2

--- loss/loss.py
+++ loss/loss.py
@@ -5,7 +5,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from loss.Gradient import gradient 
+from model.Gradient import gradient 
+
+class L1_Charbonnier_loss(nn.Module):
+    def __init__(self):
+        super(L1_Charbonnier_loss, self).__init__()
+        self.eps = 1e-6
+
+    def forward(self, sr, hr):
+        diff = torch.add(sr, -hr)
+        error = torch.sqrt( diff * diff + self.eps)
+        loss = torch.sum(error)
+        return loss
 
 class ReconstructionLoss(nn.Module):
     def __init__(self):
@@ -22,68 +33,85 @@ class GradientLoss(nn.Module):
         self.loss = nn.L1Loss()
 
     def forward(self, sr, hr):
-        sr_g = self.gradient((sr+1) * 127.5)
-        hr_g = self.gradient((hr+1) * 127.5)
-        return self.loss(sr_g, hr_g)
+        sr_g = self.gradient((sr+1) /2)
+        hr_g = self.gradient((hr+1) /2)
+        return  self.loss(sr_g, hr_g)
     
     
 class PerceptualLoss(nn.Module):
     def __init__(self):
         super(PerceptualLoss, self).__init__()
+        
     def forward(self, sr_vgg, hr_vgg):
         return F.mse_loss(sr_vgg, hr_vgg)
 
 class AdversarialLoss(nn.Module):
-    def __init__(self, logger, use_cpu = False, gan_k = 1, lr_dis = 1e-4, img_size = 160):
+    def __init__(self, logger, use_cpu = False, gan_type = 'WGAN_GP', gan_k = 1,  lr_dis = 1e-4, img_size = 160):
         super(AdversarialLoss, self).__init__()
         self.logger        = logger
+        self.gan_type      = gan_type
         self.gan_k         = gan_k
         self.device        = torch.device('cpu' if use_cpu else 'cuda')
-        self.discriminator = discriminator.Discriminator(img_size).to(self.device)
-        self.optimizer     = optim.Adam(self.discriminator.parameters(),
-                                    betas=(0, 0.9), eps=1e-8, lr=lr_dis)
-        self.bce_loss      = torch.nn.BCELoss().to(self.device)
-        
+        # self.discriminator = discriminator.Discriminator(img_size).to(self.device)
+        self.discriminator = discriminator.Discriminator().to(self.device)
+
+        if (gan_type in ['WGAN_GP', 'GAN']):
+            self.optimizer = optim.Adam(
+                self.discriminator.parameters(),
+                betas=(0, 0.9), eps=1e-8, lr=lr_dis
+            )
+        else:
+            raise SystemExit('Error: no such type of GAN!')
+
+        self.bce_loss = torch.nn.BCELoss().to(self.device)
+
+        # if (D_path):
+        #     self.logger.info('load_D_path: ' + D_path)
+        #     D_state_dict = torch.load(D_path)
+        #     self.discriminator.load_state_dict(D_state_dict['D'])
+        #     self.optimizer.load_state_dict(D_state_dict['D_optim'])
+            
     def forward(self, fake, real):
         fake_detach = fake.detach()
-        
-        # Training Discriminator k times
+
         for _ in range(self.gan_k):
             self.optimizer.zero_grad()
             d_fake = self.discriminator(fake_detach)
             d_real = self.discriminator(real)
-            
-            
-            # WGAN
-            loss_d = (d_fake - d_real).mean()
-            # real_loss = F.softplus(-d_real)
-            # fake_loss = F.softplus(d_fake)
-            # loss_d    = real_loss.mean() + fake_loss.mean()
-            
-            # Gradient Penalty
-            epsilon   = torch.rand(real.size(0), 1, 1, 1).to(self.device)
-            epsilon   = epsilon.expand(real.size())
-            hat       = fake_detach.mul(1 - epsilon) + real.mul(epsilon)
-            
-            hat.requires_grad = True
-            d_hat            = self.discriminator(hat)
-            gradients        = torch.autograd.grad(
-            outputs          = d_hat.sum(), inputs = hat,retain_graph = True, 
-                                      create_graph = True, only_inputs = True)[0]
-            gradients        = gradients.view(gradients.size(0), -1)
-            gradient_norm    = gradients.norm(2, dim = 1)
-            gradient_penalty = 10 * gradient_norm.sub(1).pow(2).mean()
-            loss_d          += gradient_penalty
-            
+            if (self.gan_type.find('WGAN') >= 0):
+                loss_d = (d_fake - d_real).mean()
+                if self.gan_type.find('GP') >= 0:
+                    epsilon = torch.rand(real.size(0), 1, 1, 1).to(self.device)
+                    epsilon = epsilon.expand(real.size())
+                    hat = fake_detach.mul(1 - epsilon) + real.mul(epsilon)
+                    hat.requires_grad = True
+                    d_hat = self.discriminator(hat)
+                    gradients = torch.autograd.grad(
+                        outputs=d_hat.sum(), inputs=hat,
+                        retain_graph=True, create_graph=True, only_inputs=True
+                    )[0]
+                    gradients = gradients.view(gradients.size(0), -1)
+                    gradient_norm = gradients.norm(2, dim=1)
+                    gradient_penalty = 10 * gradient_norm.sub(1).pow(2).mean()
+                    loss_d += gradient_penalty
+
+            elif (self.gan_type == 'GAN'):
+                valid_score = torch.ones(real.size(0), 1).to(self.device)
+                fake_score = torch.zeros(real.size(0), 1).to(self.device)
+                real_loss = self.bce_loss(torch.sigmoid(d_real), valid_score)
+                fake_loss = self.bce_loss(torch.sigmoid(d_fake), fake_score)
+                loss_d = (real_loss + fake_loss) / 2.
+
             # Discriminator update
             loss_d.backward()
             self.optimizer.step()
-        
-            # nn.utils.clip_grad_norm_(self.discriminator.parameters(), 5.0)
 
         d_fake_for_g = self.discriminator(fake)
-        loss_g     = -d_fake_for_g.mean()
-        # loss_g       = F.softplus(-d_fake_for_g).mean()
+        if (self.gan_type.find('WGAN') >= 0):
+            loss_g = -d_fake_for_g.mean()
+        elif (self.gan_type == 'GAN'):
+            loss_g = self.bce_loss(torch.sigmoid(d_fake_for_g), valid_score)
+
         # Generator loss
         return loss_g
   
@@ -92,7 +120,6 @@ class AdversarialLoss(nn.Module):
         D_optim_state_dict = self.optimizer.state_dict()
         return D_state_dict, D_optim_state_dict
 
-
 def get_loss_dict(args, logger):
     loss = {}
 
@@ -100,5 +127,5 @@ def get_loss_dict(args, logger):
     loss['grd_loss'] = GradientLoss()
     loss['per_loss'] = PerceptualLoss()
     loss['adv_loss'] = AdversarialLoss(logger = logger, use_cpu = args.cpu, gan_k = args.GAN_k, 
-                                       lr_dis = args.lr_rate_dis, img_size = args.img_training_size)
+                                       lr_dis = args.lr_rate_dis, gan_type = args.GAN_type,  img_size = args.img_training_size)
     return loss
